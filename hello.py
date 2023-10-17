ddfafc545869531290b2da7941bca00bff9b1b6e
jborghaerts <79208647+jborghaerts@users.noreply.github.com>
2023-10-18 00:43:51 +0200

    push

---
+++ hello.py
@@ -0,0 +1,13 @@
+# Shamelessly copied from http://flask.pocoo.org/docs/quickstart/
+
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hello World!'
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=8080)  # Replace '5000' with the port you want to use
+
+
